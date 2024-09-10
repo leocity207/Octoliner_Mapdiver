b9ac91513dc3469e2f9113f544f163d8447056c4
leo <l.nium@hotmail.fr>
2024-09-10 23:47:31 +0200

    add config file

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+inline const char* GET_COMMIT_HASH() {return "ec5a66699dfac62a4d1313ddb0143f600c4fd84c"; };
+inline const char* GET_VERSION() {return "1.0"; };
+inline const char* RESOURCE_PATH() {return "C:/dev/websiteR2R/resources/"; };
+
+
+#endif
